c8a9e744dfe7bb5a4e90410ee0a0e7debdd9e6ab
Kalimaha <GeoBricks2012>
2014-03-09 12:21:53 +0100

    Read datasources at the startup.

---
+++ src/main/java/org/fao/fenix/dissertation/services/bean/DatasourceBean.java
@@ -0,0 +1,81 @@
+package org.fao.fenix.dissertation.services.bean;
+
+/**
+ * @author <a href="mailto:guido.barbaglia@fao.org">Guido Barbaglia</a>
+ * @author <a href="mailto:guido.barbaglia@gmail.com">Guido Barbaglia</a>
+ */
+public class DatasourceBean {
+
+    private String id;
+
+    private String baseUrl;
+
+    private int startIdx;
+
+    private int endIdx;
+
+    private int minYear;
+
+    private int maxYear;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public int getStartIdx() {
+        return startIdx;
+    }
+
+    public void setStartIdx(int startIdx) {
+        this.startIdx = startIdx;
+    }
+
+    public int getEndIdx() {
+        return endIdx;
+    }
+
+    public void setEndIdx(int endIdx) {
+        this.endIdx = endIdx;
+    }
+
+    public int getMinYear() {
+        return minYear;
+    }
+
+    public void setMinYear(int minYear) {
+        this.minYear = minYear;
+    }
+
+    public int getMaxYear() {
+        return maxYear;
+    }
+
+    public void setMaxYear(int maxYear) {
+        this.maxYear = maxYear;
+    }
+
+    @Override
+    public String toString() {
+        return "DatasourceBean{" +
+                "id='" + id + '\'' +
+                ", baseUrl='" + baseUrl + '\'' +
+                ", startIdx=" + startIdx +
+                ", endIdx=" + endIdx +
+                ", minYear=" + minYear +
+                ", maxYear=" + maxYear +
+                '}';
+    }
+
+}
--- src/main/java/org/fao/fenix/dissertation/services/utils/ConfigReader.java
+++ src/main/java/org/fao/fenix/dissertation/services/utils/ConfigReader.java
@@ -1,10 +1,12 @@
 package org.fao.fenix.dissertation.services.utils;
 
-import org.apache.log4j.Logger;
+import com.google.gson.stream.JsonReader;
+import org.fao.fenix.dissertation.services.bean.DatasourceBean;
 import org.springframework.core.io.Resource;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:guido.barbaglia@fao.org">Guido Barbaglia</a>
@@ -14,7 +16,10 @@ public class ConfigReader {
 
     private String configPath;
 
+    private Map<String, DatasourceBean> datasources;
+
     public ConfigReader(Resource configPath) throws Exception {
+        datasources = new HashMap<String, DatasourceBean>();
         try {
             this.setConfigPath(configPath.getFile().getPath());
         } catch (IOException e) {
@@ -22,8 +27,48 @@ public class ConfigReader {
         }
     }
 
-    public void init() {
-        System.out.println("adsadasdas");
+    public void init() throws IOException {
+        File root = new File(this.configPath);
+        File[] files = root.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            InputStream is = new FileInputStream(files[i].getAbsoluteFile());
+            JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+            reader.beginArray();
+            while (reader.hasNext()) {
+                DatasourceBean b = readMessage(reader);
+                this.datasources.put(b.getId(), b);
+            }
+            reader.endArray();
+        }
+    }
+
+    public DatasourceBean readMessage(JsonReader reader) throws IOException {
+        DatasourceBean b = new DatasourceBean();
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equalsIgnoreCase("id")) {
+                b.setId(reader.nextString());
+            } else if (name.equalsIgnoreCase("baseUrl")) {
+                b.setBaseUrl(reader.nextString());
+            } else if (name.equalsIgnoreCase("startIdx")) {
+                b.setStartIdx(reader.nextInt());
+            } else if (name.equalsIgnoreCase("endIdx")) {
+                b.setEndIdx(reader.nextInt());
+            } else if (name.equalsIgnoreCase("minYear")) {
+                b.setMinYear(reader.nextInt());
+            } else if (name.equalsIgnoreCase("maxYear")) {
+                b.setMaxYear(reader.nextInt());
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return b;
+    }
+
+    public DatasourceBean getDatasource(String id) {
+        return this.datasources.get(id.toUpperCase());
     }
 
     public void setConfigPath(String configPath) {
