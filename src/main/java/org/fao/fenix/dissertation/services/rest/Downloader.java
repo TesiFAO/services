27ef9997b6b776a5b64e6f280b63a22b914670e4
Kalimaha <GeoBricks2012>
2014-03-09 14:58:09 +0100

    Download remote file

---
+++ src/main/java/org/fao/fenix/dissertation/services/rest/Downloader.java
@@ -0,0 +1,73 @@
+package org.fao.fenix.dissertation.services.rest;
+
+import org.apache.log4j.Logger;
+import org.fao.fenix.dissertation.services.bean.DatasourceBean;
+import org.fao.fenix.dissertation.services.utils.ConfigReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * @author <a href="mailto:guido.barbaglia@fao.org">Guido Barbaglia</a>
+ * @author <a href="mailto:guido.barbaglia@gmail.com">Guido Barbaglia</a>
+ */
+@Component
+@Path("/download")
+public class Downloader {
+
+    @Autowired
+    private ConfigReader configReader;
+
+    private Logger L = Logger.getLogger(Downloader.class);
+
+    @GET
+    @Path("/modis")
+    public Response getModis() throws Exception {
+
+        /* Compute result */
+        DatasourceBean modis = configReader.getDatasource("MODIS");
+
+        String path = modis.getBaseUrl() + "/2000/049/MOD13A2.A2000049.h00v08.005.2006270203425.hdf";
+        URL url = new URL(path);
+        String userAgent = "Opera/9.63 (Windows NT 5.1; U; en) Presto/2.1.1";
+        downloadFromUrl(url, "/Users/simona/Desktop/test.hdf", userAgent);
+
+        /* Stream result */
+        return Response.status(200).entity("OK").build();
+
+    }
+
+    private void downloadFromUrl(URL url, String localFilename, String userAgent) throws IOException {
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            URLConnection urlConn = url.openConnection();
+            if (userAgent != null)
+                urlConn.setRequestProperty("User-Agent", userAgent);
+            is = urlConn.getInputStream();
+            fos = new FileOutputStream(localFilename);
+            byte[] buffer = new byte[4096];
+            int len;
+            while ((len = is.read(buffer)) > 0)
+                fos.write(buffer, 0, len);
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } finally {
+                if (fos != null)
+                    fos.close();
+            }
+        }
+    }
+
+}
